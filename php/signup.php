2e401f4d136762e21e7c7a0ae689e973cfd8a69f
Michael Meyer <81069188+M1ch43lM@users.noreply.github.com>
2022-12-13 18:17:18 +0100

    Update signup.php
    Commented

--- php/signup.php
+++ php/signup.php
@@ -1,3 +1,4 @@
+// Create a new user session.
 <?php
     session_start();
     include_once "config.php";
@@ -5,11 +6,15 @@
     $lname = mysqli_real_escape_string($conn, $_POST['lname']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
+    // Checks if the fields are not empty.
     if(!empty($fname) && !empty($lname) && !empty($email) && !empty($password)){
+        // Select users by their email address
         if(filter_var($email, FILTER_VALIDATE_EMAIL)){
             $sql = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
+            // E - mail - This email already exists!
             if(mysqli_num_rows($sql) > 0){
                 echo "$email - This email already exist!";
+                // Parse jpeg png and jpg files
             }else{
                 if(isset($_FILES['image'])){
                     $img_name = $_FILES['image']['name'];
@@ -20,23 +25,29 @@
                     $img_ext = end($img_explode);
 
                     $extensions = ["jpeg", "png", "jpg"];
+                    // Checks if an image is in an array and if it is a jpeg jpg or png
                     if(in_array($img_ext, $extensions) === true){
                         $types = ["image/jpeg", "image/jpg", "image/png"];
+                        // Sets the image name.
                         if(in_array($img_type, $types) === true){
                             $time = time();
                             $new_img_name = $time.$img_name;
+                            // Inserts a new user into the database
                             if(move_uploaded_file($tmp_name,"images/".$new_img_name)){
                                 $ran_id = rand(time(), 100000000);
                                 $status = "Active now";
                                 $encrypt_pass = md5($password);
                                 $insert_query = mysqli_query($conn, "INSERT INTO users (unique_id, fname, lname, email, password, img, status)
                                 VALUES ({$ran_id}, '{$fname}','{$lname}', '{$email}', '{$encrypt_pass}', '{$new_img_name}', '{$status}')");
+                                // Select users by their email address
                                 if($insert_query){
                                     $select_sql2 = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
+                                    // Outputs a success message
                                     if(mysqli_num_rows($select_sql2) > 0){
                                         $result = mysqli_fetch_assoc($select_sql2);
                                         $_SESSION['unique_id'] = $result['unique_id'];
                                         echo "success";
+                                        // Checks if the input fields are required
                                     }else{
                                         echo "This email address not Exist!";
                                     }
