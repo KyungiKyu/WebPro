c3a110c4d9f484ee9505d77037f79d4e5db00b76
Kai Bogdanski <107174231+KyungiKyu@users.noreply.github.com>
2022-12-13 18:23:14 +0100

    commented

--- javascript/signup.js
+++ javascript/signup.js
@@ -1,20 +1,27 @@
+// Signup a user
 const form = document.querySelector(".signup form"),
 continueBtn = form.querySelector(".button input"),
 errorText = form.querySelector(".error-text");
 
+// Sets the onsubmit event.
 form.onsubmit = (e)=>{
     e.preventDefault();
 }
 
+// Signup. php
 continueBtn.onclick = ()=>{
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "php/signup.php", true);
     xhr.onload = ()=>{
+// Check if the ready state is DONE.
       if(xhr.readyState === XMLHttpRequest.DONE){
+// Send a response to the XHR.
           if(xhr.status === 200){
               let data = xhr.response;
+// Set href to user. php
               if(data === "success"){
                 location.href="users.php";
+// Sets the error text.
               }else{
                 errorText.style.display = "block";
                 errorText.textContent = data;
@@ -22,6 +29,7 @@ continueBtn.onclick = ()=>{
           }
       }
     }
+// Sends the form data to the server.
     let formData = new FormData(form);
     xhr.send(formData);
 }
