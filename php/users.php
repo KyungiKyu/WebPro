6159c7d3ba0c0c463207c11b5f44c117e05584c1
Michael Meyer <81069188+M1ch43lM@users.noreply.github.com>
2022-12-13 18:18:02 +0100

    Update users.php
    Commented

--- php/users.php
+++ php/users.php
@@ -1,11 +1,14 @@
+// Starts a chat session
 <?php
     session_start();
     include_once "config.php";
+    // Returns a list of users.
     $outgoing_id = $_SESSION['unique_id'];
     $sql = "SELECT fname,lname FROM users LEFT JOIN messages ON users.unique_id = messages.incoming_msg_id
             WHERE NOT unique_id = {$outgoing_id} ORDER BY user_id DESC";
     $query = mysqli_query($conn, $sql);
     $output = "";
+    // Outputs a formatted list of users available to chat
     if(mysqli_num_rows($query) == 0){
         $output .= "No users are available to chat";
     }elseif(mysqli_num_rows($query) > 0){
