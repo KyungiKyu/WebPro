1e7ab3cf1088a4b1598a2207724eb8b739afa5a6
Michael Meyer <81069188+M1ch43lM@users.noreply.github.com>
2022-12-13 17:51:20 +0100

    Update index.php
    Added Comments to the Code

--- Code/index.php
+++ Code/index.php
@@ -1,3 +1,4 @@
+// Starts the session if it s not already started.
 <?php 
   session_start();
   if(isset($_SESSION['unique_id'])){
@@ -5,7 +6,10 @@
   }
 ?>
 
+// Include the header.php
+
 <?php include_once "header.php"; ?>
+// Create a signup form
 <body>
   <div class="wrapper">
     <section class="form signup">
@@ -13,28 +17,34 @@
       <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
         <div class="error-text"></div>
         <div class="name-details">
+          // Create a div for the field First name
           <div class="field input">
             <label>First Name</label>
             <input type="text" name="fname" placeholder="First name" required>
           </div>
+          // Create a div for the field Last Name
           <div class="field input">
             <label>Last Name</label>
             <input type="text" name="lname" placeholder="Last name" required>
           </div>
         </div>
+        // Creates a div for the email field
         <div class="field input">
           <label>Email Address</label>
           <input type="text" name="email" placeholder="Enter your email" required>
         </div>
+        // Create a password input field
         <div class="field input">
           <label>Password</label>
           <input type="password" name="password" placeholder="Enter new password" required>
           <i class="fas fa-eye"></i>
         </div>
+        // Generates a div for the image field
         <div class="field image">
           <label>Select Image</label>
           <input type="file" name="image" accept="image/x-png,image/gif,image/jpeg,image/jpg" required>
         </div>
+        // Create the submit button and then continue to chat
         <div class="field button">
           <input type="submit" name="submit" value="Continue to Chat">
         </div>
