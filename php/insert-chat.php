596d64c24b680fd11a531333e08b3cb0f3a3db75
Michael Meyer <81069188+M1ch43lM@users.noreply.github.com>
2022-12-13 18:09:48 +0100

    Update insert-chat.php
    Commented

--- php/insert-chat.php
+++ php/insert-chat.php
@@ -1,10 +1,13 @@
+// Starts the login session.
 <?php 
     session_start();
+    // Include config.php and outgoing_id once
     if(isset($_SESSION['unique_id'])){
         include_once "config.php";
         $outgoing_id = $_SESSION['unique_id'];
         $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
         $message = mysqli_real_escape_string($conn, $_POST['message']);
+        // Insert a message into the login.php
         if(!empty($message)){
             $sql = mysqli_query($conn, "INSERT INTO messages (incoming_msg_id, outgoing_msg_id, msg)
                                         VALUES ({$incoming_id}, {$outgoing_id}, '{$message}')") or die();
