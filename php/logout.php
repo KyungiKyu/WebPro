1ccc0d62ea1172d0fd19eeab7629c7576df1eb6a
Michael Meyer <81069188+M1ch43lM@users.noreply.github.com>
2022-12-13 18:12:55 +0100

    Update logout.php
    Commented

--- php/logout.php
+++ php/logout.php
@@ -1,16 +1,21 @@
+// Starts the user logout session.
 <?php
     session_start();
+    // Include the logout config.php if it exists
     if(isset($_SESSION['unique_id'])){
         include_once "config.php";
         $logout_id = mysqli_real_escape_string($conn, $_GET['logout_id']);
+        // Update the users status
         if(isset($logout_id)){
             $status = "Offline now";
             $sql = mysqli_query($conn, "UPDATE users SET status = '{$status}' WHERE unique_id={$_GET['logout_id']}");
+            // Sets the login.php header
             if($sql){
                 session_unset();
                 session_destroy();
                 header("location: ../login.php");
             }
+            // Sets the login headers.
         }else{
             header("location: ../users.php");
         }
