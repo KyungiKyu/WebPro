3df0eeb7936488273407d8897e9ca7f2976e56b9
Kai Bogdanski <107174231+KyungiKyu@users.noreply.github.com>
2022-12-13 18:22:36 +0100

    commented

--- javascript/pass-show-hide.js
+++ javascript/pass-show-hide.js
@@ -1,10 +1,13 @@
+// Adds the active icon to the form.
 const pswrdField = document.querySelector(".form input[type='password']"),
 toggleIcon = document.querySelector(".form .field i");
 
+// Adds an icon to the toggleIcon.
 toggleIcon.onclick = () =>{
   if(pswrdField.type === "password"){
     pswrdField.type = "text";
     toggleIcon.classList.add("active");
+// Removes the active icon.
   }else{
     pswrdField.type = "password";
     toggleIcon.classList.remove("active");
