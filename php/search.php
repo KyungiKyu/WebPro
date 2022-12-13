5346b872d56119da472874e537a1243e5c498cf7
Michael Meyer <81069188+M1ch43lM@users.noreply.github.com>
2022-12-13 18:14:04 +0100

    Update search.php
    Commented

--- php/search.php
+++ php/search.php
@@ -1,13 +1,15 @@
+// Displays a list of users related to your search term
 <?php
     session_start();
     include_once "config.php";
-
+    // Generates a mysqli_real_escape_string from the current session.
     $outgoing_id = $_SESSION['unique_id'];
     $searchTerm = mysqli_real_escape_string($conn, $_POST['searchTerm']);
-
+    // Query for users with unique id.
     $sql = "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id} AND (fname LIKE '%{$searchTerm}%' OR lname LIKE '%{$searchTerm}%') ";
     $output = "";
     $query = mysqli_query($conn, $sql);
+    // include_once for mysqli_num_rows
     if(mysqli_num_rows($query) > 0){
         include_once "data.php";
     }else{
